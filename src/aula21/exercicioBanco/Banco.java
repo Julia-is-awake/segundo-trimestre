f53894b279758f17146b578f4eda811d0e9ec524
julia-is-awake <juliasorgetzalves@gmail.com>
2024-07-08 07:55:43 -0300

    terminar exercicio

--- src/aula21/exercicioBanco/Banco.java
+++ src/aula21/exercicioBanco/Banco.java
@@ -3,7 +3,7 @@ package aula21.exercicioBanco;
 import java.util.Scanner;
 
 public class Banco {
-    public static Conta cadastrarConta(char op){
+    public static ContaCorrente cadastrarContaCorrente(){
 
         Scanner ler = new Scanner(System.in);
         System.out.print("Informe o nome do titular: ");
@@ -11,32 +11,31 @@ public class Banco {
         System.out.print("Informe sua senha: ");
         String senha = ler.nextLine();
 
-        switch (op) {
-
-            case 'c' -> {
-                System.out.print("Informe o limite da conta: ");
-                float limite = ler.nextFloat();
-                Conta c1 = new ContaCorrente(titular, senha, limite);
-                System.out.println("A nova conta tem o identificador " + c1.getIdentificador());
-                System.out.print("Informe o saldo atual da sua conta: ");
-                c1.saldo = ler.nextFloat();
-                return c1;
-            }
-
-            case 'p' -> {
-                System.out.print("Insira o deposito inicial: ");
-                float deposito = ler.nextFloat();
-                Conta c1 = new ContaPoupanca(titular, senha, deposito);
-                System.out.println("A nova conta tem o identificador " + c1.getIdentificador());
-                return c1;
-            }
-
-        }
-
-        return null;
+        System.out.print("Informe o limite da conta: ");
+        float limite = ler.nextFloat();
+        ContaCorrente c1 = new ContaCorrente(titular, senha, limite);
+        System.out.println("A nova conta tem o identificador " + c1.getIdentificador());
+        System.out.print("Informe o saldo atual da sua conta: ");
+        c1.saldo = ler.nextFloat();
+        return c1;
     }
 
-    public static Conta acessandoConta(Conta c1){
+    public static ContaPoupanca cadastrarContaPoupanca(){
+
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Informe o nome do titular: ");
+        String titular = ler.nextLine();
+        System.out.print("Informe sua senha: ");
+        String senha = ler.nextLine();
+
+        System.out.print("Insira o deposito inicial: ");
+        float deposito = ler.nextFloat();
+        ContaPoupanca c1 = new ContaPoupanca(titular, senha, deposito);
+        System.out.println("A nova conta tem o identificador " + c1.getIdentificador());
+        return c1;
+    }
+
+    public static Conta acessandoContaCorrente(ContaCorrente c1){
         char op;
         do{
             System.out.print("d - depositar\ns - sacar\nv - ver saldo\ne - sair\nopção: ");
@@ -64,11 +63,32 @@ public class Banco {
         return c1;
     }
 
+    public static Conta acessandoContaPoupanca(ContaPoupanca c1){
+        char op;
+        do{
+            System.out.print("d - depositar\nv - ver saldo\ne - sair\nopção: ");
+            op = new Scanner(System.in).next().toLowerCase().charAt(0);
+            switch(op){
+                case 'd' -> {
+                    System.out.print("Qual o valor deseja depositar: ");
+                    float valor = new Scanner(System.in).nextFloat();
+                    c1.depositar(valor);
+                    System.out.println("Depósito realizado!");
+                }
+                case 'v' -> System.out.println(c1.verificaSaldo());
+            }
+        }while(op != 'e');
+
+        return c1;
+    }
+
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        Conta[] contas = new Conta[10];
+        ContaCorrente[] contasCorrente = new ContaCorrente[10];
+        ContaPoupanca[] contasPoupanca = new ContaPoupanca[10];
 
-        int countContas = 0;
+        int countContasCorrente = 0;
+        int countContasPoupanca = 0;
         char resp;
 
         do{
@@ -78,32 +98,57 @@ public class Banco {
                 case 'c' -> {
                     System.out.print("Qual o tipo de conta:\nc - conta corrente\np - conta poupança\nOpção: ");
                     char option = ler.next().toLowerCase().charAt(0);
-                    contas[countContas] = cadastrarConta(option);
-                    System.out.println("Conta cadastrado obteve o identificador "+contas[countContas].getIdentificador());
-                    countContas++;
+                    if(option == 'c'){
+                        contasCorrente[countContasCorrente] = cadastrarContaCorrente();
+                        countContasCorrente++;
+                        System.out.println("Conta cadastrado obteve o identificador "+contasCorrente[countContasCorrente].getIdentificador());
+                    }else if(option == 'p'){
+                        contasPoupanca[countContasPoupanca] = cadastrarContaPoupanca();
+                        countContasPoupanca++;
+                    }
+                    System.out.println("Conta cadastrado obteve o identificador "+contasPoupanca[countContasPoupanca].getIdentificador());
                 }
                 case 'a'->{
                     System.out.print("Digite o identificador: ");
                     String id = ler.next();
                     System.out.print("Digite a senha: ");
                     String senha = ler.next();
+                    System.out.print("c - conta corrente\np - conta poupança\nDigite a opção de conta: ");
+                    char contaOp = ler.next().toLowerCase().charAt(0);
                     int posicao = -1;
-                    for (int i = 0; i< countContas; i++) {
-                        if (contas[i].getIdentificador().equals(id) && contas[i].getSenha().equals(senha)) {
-                            posicao = i;
+                    if(contaOp == 'c'){
+                        for (int i = 0; i< countContasCorrente; i++) {
+                            if (contasCorrente[i].getIdentificador().equals(id) && contasCorrente[i].getSenha().equals(senha)) {
+                                posicao = i;
+                            }
+                        }
+                    }else if(contaOp == 'p'){
+                        for (int i = 0; i< countContasPoupanca; i++) {
+                            if (contasPoupanca[i].getIdentificador().equals(id) && contasPoupanca[i].getSenha().equals(senha)) {
+                                posicao = i;
+                            }
                         }
                     }
                     if (posicao >= 0) {
-                        contas[posicao] = acessandoConta(contas[posicao]);
+                        if(contaOp == 'c'){
+                            contasCorrente[posicao] = (ContaCorrente) acessandoContaCorrente(contasCorrente[posicao]);
+                        }else if(contaOp == 'p'){
+                            contasPoupanca[posicao] = (ContaPoupanca) acessandoContaPoupanca(contasPoupanca[posicao]);
+                        }
                     } else {
                         System.out.println("Dados de acesso inválidos!");
                     }
                 }
                 case 'l' -> {
                     System.out.println("Listagem de contas...");
-                    for(int i = 0; i < countContas; i++){
-                        System.out.println("Títular: "+contas[i].getTitular());
-                        System.out.println("Identificador: "+contas[i].getIdentificador());
+                    for(int i = 0; i < countContasCorrente; i++){
+                        System.out.println("Títular: "+contasCorrente[i].getTitular());
+                        System.out.println("Identificador: "+contasCorrente[i].getIdentificador());
+                        System.out.println("-----------------------");
+                    }
+                    for(int i = 0; i < countContasPoupanca; i++){
+                        System.out.println("Títular: "+contasPoupanca[i].getTitular());
+                        System.out.println("Identificador: "+contasPoupanca[i].getIdentificador());
                         System.out.println("-----------------------");
                     }
                 }
